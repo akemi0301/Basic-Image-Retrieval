8de891076a6d1be3039a87d8d924371aee6adcd1
Diễm Sương <85627308+DiemSuong0412@users.noreply.github.com>
2022-11-05 14:19:36 +0700

    Update fe_all_dataset.py

--- utils/fe_all_dataset.py
+++ utils/fe_all_dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 from tensorflow.keras.preprocessing import image as kimage
 
 root_img_path = "../static/img/"
-root_fearure_path = "../static/feature/"
+root_feature_path = "../static/feature/"
 dic_categories = ['scenery', 'furniture', 'animal', 'plant']
 
 if __name__ == '__main__':
@@ -17,11 +17,11 @@ if __name__ == '__main__':
     #     if path.endswith(".npz"):
     #         print(path)
 
-    for path in [root_fearure_path + path for path in os.listdir(root_fearure_path) if path.endswith(".npz")]:
+    for path in [root_feature_path + path for path in os.listdir(root_feature_path) if path.endswith(".npz")]:
         data = np.load(path)
         paths_feature.extend(data["array1"])
         imgs_feature.extend(data["array2"])
 
-    np.savez_compressed(root_fearure_path+"all_feartures", array1=np.array(paths_feature), array2=np.array(imgs_feature))
+    np.savez_compressed(root_feature_path+"all_feartures", array1=np.array(paths_feature), array2=np.array(imgs_feature))
     print("Done!")
 

dfbb6eacb6abdbb8ce19fb6c453934bc67da5e2f
Diễm Sương <85627308+DiemSuong0412@users.noreply.github.com>
2022-11-05 14:17:55 +0700

    Update fe_all_dataset.py

--- utils/fe_all_dataset.py
+++ utils/fe_all_dataset.py
@@ -8,27 +8,6 @@ root_img_path = "../static/img/"
 root_fearure_path = "../static/feature/"
 dic_categories = ['scenery', 'furniture', 'animal', 'plant']
 
-def folder_to_images(folder):
-    
-    list_dir = [folder + '/' + name for name in os.listdir(folder) if name.endswith((".jpg", ".png", ".jpeg"))]
-    
-    i = 0
-    images_np = np.zeros(shape=(len(list_dir), 224, 224, 3))
-    images_path = []
-    for path in list_dir:
-        try:
-            img = kimage.load_img(path, target_size=(224, 224))
-            images_np[i] = kimage.img_to_array(img, dtype=np.float32)
-            images_path.append(path)
-            i += 1
-            
-        except Exception:
-            print("error: ", path)
-    #         os.remove(root_img_path + img_path)
-
-    images_path = np.array(images_path)
-    return images_np, images_path
-
 if __name__ == '__main__':
     
     imgs_feature = []

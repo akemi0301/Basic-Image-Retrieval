75c19e93acde322ef1ddaded1ab0f95e7bee4e2f
tryphena.leii <88385496+BluzOtter@users.noreply.github.com>
2022-11-21 16:42:11 +0700

    add  compare class name image and change UI web

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import numpy as np
 from utils.model import FeatureExtractor
 from datetime import datetime
-import os
+import pandas as pd
 from tensorflow.keras.preprocessing import image as kimage
 from PIL import Image
+from utils.faiss_search import search_vector
 
 from flask import Flask, url_for, render_template, request, redirect, session
 # from flask_ngrok import run_with_ngrok
@@ -11,25 +12,51 @@ from flask import Flask, url_for, render_template, request, redirect, session
 # Read image features
 fe = FeatureExtractor()
 
-
-def cosine_similarity(query, X):
-    norm_2_query = np.sqrt(np.sum(query*query))
-    norm_2_X = np.sqrt(np.sum(X*X, axis=-1))
-    return np.sum(query*X, axis=-1)/(norm_2_query*norm_2_X)
-
-def retrieval_images(query_vector, imgs_feature):
-    # caculate similarity between query and features in database
-    rates = cosine_similarity(query_vector, imgs_feature)
-    id_s = np.argsort(-rates)[:100] # Top 30 results
-    
-    return [(round(rates[id], 2), paths_feature[id]) for id in id_s]
-
 folder_query = "query_pic/"
-root_fearure_path = "static/feature/all_feartures.npz"
+root_fearure_path = "./static/feature/all_features.npz"
 
 data = np.load(root_fearure_path)
-paths_feature = data["array1"]
-imgs_feature = data["array2"]
+
+paths_feature = data["paths_feature"]
+# print('path images = ',len(paths_feature))
+
+imgs_feature = data["imgs_feature"]
+# print('Image features = ', len(imgs_feature))
+
+class_imgs = data["class_imgs"]
+
+def search_and_evalution(img_query):
+
+    topk = 100
+    # retrieval_images
+    scores, ids, paths  = search_vector(img_query, topk)
+    
+    # re-ranking  
+    result = pd.DataFrame()
+    result['path'] = paths_feature[[idx for idx in ids]]
+    result['score'] = scores[[s for s in range(len(scores))]][0]
+    result['rank'] = result['score'].rank(ascending = 1)
+    result['class_img_name'] = class_imgs[[idx for idx in ids]]
+    result = result.sort_index()
+
+    class_name_top1 = result['class_img_name'][0]
+
+    content_compare = []
+    for cls in result['class_img_name']:
+        if str(cls) == class_name_top1:
+            content_compare.append(True)
+        else:
+            content_compare.append(False)
+    
+    result['class_compare'] = content_compare
+
+    correct_result=content_compare.count(True)
+
+    precision = correct_result/len(content_compare)
+
+    print('Precision:',precision)
+
+    return result, precision
 
 app = Flask(__name__)
 
@@ -45,23 +72,23 @@ def index():
         img.save(uploaded_img_path)
         
         # Load query image and FeatureExtractor
-        query = kimage.load_img(uploaded_img_path, target_size=(224, 224))
-        query = kimage.img_to_array(query, dtype=np.float32)
-        query_vector = fe.extract(query[None, :])
+        # query = kimage.load_img(uploaded_img_path, target_size=(224, 224))
+        # query = kimage.img_to_array(query, dtype=np.float32)
+        # query_vector = fe.extract(query[None, :])
 
-        # retrieval_images
-        scores = retrieval_images(query_vector, imgs_feature)
-
-
+        result, presicion = search_and_evalution(uploaded_img_path)
+        # Lấy kết quả và gửi đến html
+        rs = result[['path','class_compare']]  
+        rs = rs.to_records(index=False)
+        rs = list(rs)
+        precision = "Precision: "+str(presicion)
         return render_template('index_thumb.html',
-                               query_path=uploaded_img_path,
-                               scores1=scores[:10],
-                               scores2=scores[10:20],
-                               scores3=scores[20:])
-
-    return render_template('index_thumb.html')
-
-
+                            query_path=uploaded_img_path,
+                            scores=rs,
+                            precision = precision)
+    else:
+        return render_template('index_thumb.html')
+        
 if __name__=="__main__":
     
     app.run(host='localhost', port='6868', debug=True)

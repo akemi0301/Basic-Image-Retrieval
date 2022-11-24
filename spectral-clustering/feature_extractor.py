d6398a7f856009c1291f969ec27c6832ba1ebb9d
Sương Sương <19522145@gm.uit.edu.vn>
2022-11-24 23:04:08 +0700

    update feature extractor

--- spectral-clustering/feature_extractor.py
+++ spectral-clustering/feature_extractor.py
@@ -1,44 +1,41 @@
 import os
 import numpy as np
 import pandas as pd
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing import image as kimage
 from model import FeatureExtractor
 
 root_img_path = "./static/img/"
 root_feature_path = "./static/feature/"
 dic_categories = ['animal', 'furniture', 'plant', 'scenery']
 
-def folder_to_images(folder, label):
+def folder_to_images(folder):
 
     list_dir = [folder + '/' + name for name in os.listdir(folder) if name.endswith((".jpg", ".png", ".jpeg"))]
-    
     i = 0
     images_np = np.zeros(shape=(len(list_dir), 224, 224, 3))
+    folder_name = []
     images_path = []
-    labels = []
     for path in list_dir:
         try:
-            img = image.load_img(path, target_size=(224, 224))
-            images_np[i] = image.img_to_array(img, dtype=np.float32)
-            images_np[i] = np.expand_dims(images_np[i], axis=0)
+            img = kimage.load_img(path, target_size=(224, 224))
+            images_np[i] = kimage.img_to_array(img, dtype=np.float32)
             images_path.append(path)
-            labels.append(label)
+            folder_name.append(path.split('/')[-2])
             i += 1
-            
         except Exception:
             print("error: ", path)
-
-
-    return images_np, images_path, labels
-
+    return images_np, images_path, folder_name
 
 if __name__ == '__main__':
 
     fe = FeatureExtractor()
+
     for folder in os.listdir(root_img_path):
         if folder.split("_")[0] in dic_categories:
-            label = folder.split("_")[1]
             path = root_img_path + folder
-            images_np, images_path, labels = folder_to_images(path, label)
-            print(root_feature_path+folder)
-            np.savez_compressed(root_feature_path+folder, array1=np.array(images_path), array2=fe.extract(images_np), array3=np.array(labels))
+            images_np, images_path, folder_name = folder_to_images(path)
+            np.savez_compressed(root_feature_path+folder, array1=np.array(images_path), array2=fe.extract(images_np), array3=np.array(folder_name))
+
+    # path = './static/images/animal_Alligato/0.89070600258874.jpg'
+    # p  = path.split('/')[-2]
+    # print(p)

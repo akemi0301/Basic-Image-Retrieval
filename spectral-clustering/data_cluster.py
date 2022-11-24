b302a17defbaec14db25c30e4090360378e4d70c
Sương Sương <19522145@gm.uit.edu.vn>
2022-11-24 23:08:07 +0700

    update data cluster

--- spectral-clustering/data_cluster.py
+++ spectral-clustering/data_cluster.py
@@ -1,77 +1,37 @@
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import kneighbors_graph
-from scipy import sparse, linalg
-from sklearn.cluster import KMeans
-from pathlib import Path
+from sklearn.cluster import SpectralClustering
 
 # load features
-# df= pd.read_csv('./static/feature/features_img.csv') 
-
 root_feature_path = "./static/feature/all_features.npz"
-
 data = np.load(root_feature_path)
 
-df = pd.DataFrame(data["array2"])
-df['label'] = data["array3"]
-df['img_path'] = data["array1"]
+print(len(data["imgs_feature"]))
+print(data["imgs_feature"].shape)
+
+df = pd.DataFrame(data["imgs_feature"])
+df['label'] = data["class_imgs"]
+df['img_path'] = data["paths_feature"]
+
+print(df.describe(include="all"))
 
 #phan cum features bang thuat toan spectral clustering
-#tinh ma tran lalapcian
-def generate_graph_laplacian(df, nn):
-    """Generate graph Laplacian from data."""
-    # Adjacency Matrix.
-    connectivity = kneighbors_graph(X=df, n_neighbors=nn, mode='connectivity')
-    adjacency_matrix_w = (1/2)*(connectivity + connectivity.T)
-    # Graph Laplacian.
-    graph_laplacian_s = sparse.csgraph.laplacian(csgraph=adjacency_matrix_w, normed=False)
-    graph_laplacian = graph_laplacian_s.toarray()
-    return graph_laplacian 
+spec_cl = SpectralClustering(
+    n_clusters=40, 
+    n_neighbors=8,
+    random_state=25,
+    assign_labels='kmeans',
+    affinity='nearest_neighbors'
+)
 
-#tinh gia tri rieng, vector rieng
-def compute_spectrum_graph_laplacian(graph_laplacian):
-    """Compute eigenvalues and eigenvectors and project 
-    them onto the real numbers.
-    """
-    eigenvals, eigenvcts = linalg.eig(a=graph_laplacian)
-    eigenvals = np.real(eigenvals)
-    eigenvcts = np.real(eigenvcts)
-    return eigenvals, eigenvcts
-
-#chon k vector rieng dau tien
-def project_and_transpose(eigenvals, eigenvcts, num_ev):
-    """Select the eigenvectors corresponding to the first 
-    (sorted) num_ev eigenvalues as columns in a data frame.
-    """
-    eigenvals_sorted_indices = np.argsort(eigenvals)
-    indices = eigenvals_sorted_indices[: num_ev]
-    proj_df = pd.DataFrame(eigenvcts[:, indices.squeeze()])
-    proj_df.columns = ['v_' + str(c) for c in proj_df.columns]
-    return proj_df
-
-
-#phan cum voi kmeans
-def run_k_means(df, n_clusters):
-    """K-means clustering."""
-    k_means = KMeans(n_clusters=n_clusters)
-    k_means.fit(df)
-    cluster = k_means.predict(df)
-    return cluster
-
-#Spectral clustering
-def spectral_clustering(df, n_neighbors, num_ev, n_clusters):
-    """Spectral Clustering Algorithm."""
-    graph_laplacian = generate_graph_laplacian(df, n_neighbors)
-    eigenvals, eigenvcts = compute_spectrum_graph_laplacian(graph_laplacian=graph_laplacian)
-    proj_df = project_and_transpose(eigenvals, eigenvcts, num_ev)
-    cluster = run_k_means(proj_df, n_clusters)
-    return cluster
-
-# #phan cum tinh centroids 
-cluster = spectral_clustering(df=df[df.columns[0:2048]], n_neighbors=8,num_ev=30, n_clusters=61)
+cluster = spec_cl.fit_predict(df[df.columns[0:2048]])
 df['cluster'] = pd.Series(cluster, index=df.index)
+# print(df["cluster"])
 Centroids = df.groupby(["cluster"]).mean()
+# print(Centroids)
+
+# labels = df[['label', 'cluster']].groupby(['label']).agg(num_cluster = ('cluster' , 'count'), name_cluster = ('cluster' ,  'unique'))
 
 # #save data
-df.to_csv("./static/feature/clusters.csv", index = False)
-Centroids.to_csv("./static/feature/centroids.csv", index = False)
+df.to_csv("./static/cluster/clusters_40.csv", index = False)
+Centroids.to_csv("./static/cluster/centroids_40.csv", index = False)

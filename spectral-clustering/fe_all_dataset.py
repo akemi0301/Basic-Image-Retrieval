da4957b9d32aeced1816d643db62c761dd328410
Sương Sương <19522145@gm.uit.edu.vn>
2022-11-24 23:06:02 +0700

    update feature all

--- spectral-clustering/fe_all_dataset.py
+++ spectral-clustering/fe_all_dataset.py
@@ -1,4 +1,3 @@
-
 import os
 import numpy as np
 from pathlib import Path
@@ -11,16 +10,16 @@ if __name__ == '__main__':
     
     imgs_feature = []
     paths_feature = []
-    imgs_label = []
-
-    # for path in os.listdir(root_fearure_path):
-    #     if path.endswith(".npz"):
-    #         print(path)
+    class_imgs = []
 
     for path in [root_feature_path + path for path in os.listdir(root_feature_path) if path.endswith(".npz")]:
         data = np.load(path)
         paths_feature.extend(data["array1"])
         imgs_feature.extend(data["array2"])
-        imgs_label.extend(data["array3"])
-    np.savez_compressed(root_feature_path+"all_features", array1=np.array(paths_feature), array2=np.array(imgs_feature), array3=np.array(imgs_label))
+
+    for path in paths_feature:
+        class_img = path.split("/")[-2]
+        class_imgs.append(class_img)
+
+    np.savez_compressed(root_feature_path+"all_features", paths_feature=np.array(paths_feature), imgs_feature=np.array(imgs_feature), class_imgs=np.array(class_imgs))
     print("Done!")

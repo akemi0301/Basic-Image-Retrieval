12bc2f85696bd2a1d2a2ad9fe288fc2864fa300e
Sương Sương <19522145@gm.uit.edu.vn>
2022-11-28 02:46:28 +0700

    update app

--- spectral-clustering/app.py
+++ spectral-clustering/app.py
@@ -1,68 +1,98 @@
 import numpy as np
 import pandas as pd
 from PIL import Image
+from time import time
 from datetime import datetime
 from tensorflow.keras.preprocessing import image
 from flask import Flask, request, render_template
 from model import FeatureExtractor
 from sklearn.metrics.pairwise import cosine_similarity
+from product_quantization.pq import PQ
 
+pq = PQ()
 fe = FeatureExtractor()
 
-# Đọc vectors từ file csv
 global_df_vectors = pd.read_csv('./static/cluster/clusters_30.csv')
-# Đọc centroids từ file csv
 global_centroids = pd.read_csv('./static/cluster/centroids_30.csv')
 
-#Tra cứu ảnh và đánh giá 
-def search_evaluate(search_vector, global_df_vectors, global_centroids):
+feature_path = "./static/feature/feature_pq.npz"
+data_pq = np.load(feature_path)
+codeword = data_pq['array1']
+pqcode = data_pq['array2']
 
-  # Đọc vectors từ file csv
-  df_vectors = global_df_vectors
-  # Đọc centroids từ file csv
-  centroids = global_centroids
-  # Tính khoảng cách giữa các tâm cụm và điểm ảnh truy vấn
-  # distance = np.linalg.norm(np.array(centroids[centroids.columns[0:2048]])- search_vector, axis=1)
-  distance = np.linalg.norm(1- cosine_similarity(centroids[centroids.columns[0:2048]], search_vector),axis =1)
-  print(distance)
+root_feature_path = "./static/feature/all_features.npz"
+data = np.load(root_feature_path)
+label = data['class_imgs']
+df = pd.DataFrame(data["imgs_feature"])
+df['label'] = data["class_imgs"]
+df['img_path'] = data["paths_feature"]
 
-  #Lấy tên cluster có tâm cụm min
-  min_cluster = list(distance).index(np.min(distance))
-  print(min_cluster)
-
-  #Lấy ra các cụm gần nhất với điểm ảnh truy vấn
-  df_vectors = df_vectors[df_vectors["cluster"]== min_cluster]
-  print(df_vectors.describe(include="all"))
-
-  # Tính khoảng cách giữ điểm ảnh truy vấn và các điểm trong cụm gần nhất đó
-  # distance = np.linalg.norm(np.array(df_vectors[df_vectors.columns[0:2048]])- search_vector, axis=1)
-  distance = np.linalg.norm(1- cosine_similarity(df_vectors[df_vectors.columns[0:2048]], search_vector),axis =1)
-  df_vectors['distance'] = pd.Series(distance, index=df_vectors.index)
-  
-  # Sắp xếp lại các điểm trong cụm theo thứ tự tăng dần của khoảng cách tới điểm ảnh truy vấn
-  df_vectors['rank'] = df_vectors['distance'].rank(ascending = 1)
-  df_vectors = df_vectors.set_index('rank')
-  df_vectors = df_vectors.sort_index()
-
-  #Lấy ra kết quả tối đa 100 ảnh giống nhất với ảnh query trong cluster
-  result = df_vectors[0:100] 
-
-  content_image = result['label'].iloc[0]
-  print(content_image)
+# Đánh giá
+def search_evaluate(df, query, option = 0):
+  if (option == 0): 
+    result = direct_search(df, query)
+  elif (option == 1):
+    result = spectral_clustering_search(query, global_df_vectors, global_centroids)
+  elif (option==2):
+    result = product_quantization_search(df, query, codeword, pqcode)
 
   #So sánh với nhãn để đánh giá true/false
+  content_image = result['label'].iloc[0]
+  # print(content_image)
   content_compare = []
   for content in result['label']:
     if str(content) == content_image:
       content_compare.append(True)
     else:
       content_compare.append(False)
+
   result['Content_compare'] = pd.Series(content_compare, index=result.index)
   correct_result=content_compare.count(True)
   precision = correct_result/len(content_compare)
-  print('Precision:',precision)
-  return result, precision
+  # print('Precision:',precision)
 
+  rs = result[['img_path','Content_compare']]  
+  rs = rs.to_records(index=False)
+  rs = list(rs)
+  return rs, precision
+
+#Tra cứu ảnh trực tiếp từ vector đặc trưng
+def direct_search(df, query_vector):
+    df_vectors = df
+    distance = np.linalg.norm(1- cosine_similarity(df_vectors[df_vectors.columns[0:2048]], query_vector),axis =1)
+    df_vectors['distance'] = pd.Series(distance, index=df_vectors.index)
+    df_vectors['rank'] = df_vectors['distance'].rank(ascending = 1)
+    df_vectors = df_vectors.set_index('rank')
+    df_vectors = df_vectors.sort_index()
+    result = df_vectors[0:100]
+    return result
+
+#Tra cứu ảnh sử dụng thuật toán phân cụm
+def spectral_clustering_search(search_vector, global_df_vectors, global_centroids):
+  df_vectors = global_df_vectors
+  centroids = global_centroids
+  distance = np.linalg.norm(1- cosine_similarity(centroids[centroids.columns[0:2048]], search_vector),axis =1)
+  min_cluster = list(distance).index(np.min(distance))
+  df_vectors = df_vectors[df_vectors["cluster"]== min_cluster]
+  distance = np.linalg.norm(1- cosine_similarity(df_vectors[df_vectors.columns[0:2048]], search_vector),axis =1)
+  df_vectors['distance'] = pd.Series(distance, index=df_vectors.index)
+  df_vectors['rank'] = df_vectors['distance'].rank(ascending = 1)
+  df_vectors = df_vectors.set_index('rank')
+  df_vectors = df_vectors.sort_index()
+  result = df_vectors[0:100]
+  return result
+
+#Tra cứu ảnh sử dụng thuật toán product quanlization
+def product_quantization_search(df, query, codeword, pqcode):
+    query_vector = query[0]
+    dist = pq.search(codeword, pqcode, query_vector)
+    df_vectors = df
+    df_vectors['distance'] = pd.Series(dist, index=df_vectors.index)
+    df_vectors['rank'] = df_vectors['distance'].rank(ascending = 1)
+    df_vectors = df_vectors.set_index('rank')
+    df_vectors = df_vectors.sort_index()
+    result = df_vectors[0:100]
+    return result
 
 # build web Flask
 app = Flask(__name__)
@@ -82,22 +112,26 @@ def index():
         # Load query image and FeatureExtractor
         query = image.load_img(uploaded_img_path, target_size=(224, 224))
         query = image.img_to_array(query, dtype=np.float32)
-        query_vector = fe.extract(query[None, :])
+        query = fe.extract(query[None, :])
 
         #Tra cứu ảnh và đánh giá
-        result, ps = search_evaluate(query_vector, global_df_vectors, global_centroids)
+        start_time = datetime.now()
+        result, ps = search_evaluate(df, query, 0)
+        end_time = datetime.now()
+        time = end_time - start_time
+        print("Time: ", time, "seconds")
+
         # Lấy kết quả và gửi đến html
-        rs = result[['img_path','Content_compare']]  
-        rs = rs.to_records(index=False)
-        rs = list(rs)
         precision = "Precision: "+str(ps)
+        time = "Time: "+str(time)+ "seconds"
 
-        return render_template('index_thumb.html',
+        return render_template('index.html',
                             query_path=uploaded_img_path,
-                            scores=rs,
-                            precision = precision)
+                            scores = result,
+                            precision = precision,
+                            time = time)
 
-    return render_template('index_thumb.html')
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
